48c8ff6f03d20885cdfc3552d89c5018e02d42c7
akazyulin <akazyulin@i-core.ru>
2022-05-18 06:18:21 -0400

    Refactor to volume from dir mount, dumps deleted

--- roles/psql_docker/files/03_clean_db_script.sql
+++
@@ -1,7 +0,0 @@
-TRUNCATE racks.racks.dc CASCADE;
-TRUNCATE racks.racks.operations_history;
-TRUNCATE racks.racks.migrations;
-TRUNCATE racks.racks.uaggregate CASCADE;
-DELETE
-FROM racks.racks.uaggregation_type ut
-WHERE ut."name" NOT IN ('HwunitAgg','HwbuiltinAgg','AggAgg');
